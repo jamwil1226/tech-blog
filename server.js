54d683bac6955497cf5b6006c8aa4e530d2ddaab
Jamie Williams <jamwil1226@yahoo.com>
2021-11-24 12:02:55 -0500

    login logout info

--- server.js
+++ server.js
@@ -7,6 +7,22 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
